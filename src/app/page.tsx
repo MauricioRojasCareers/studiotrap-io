404c949b5b608b604b2bfdda9295e95a73e0885e
Mauricio <mauriciorojascareers@gmail.com>
2024-08-17 17:36:27 -0500

    file formatting

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Main() {
-  return (
-    <div className="justify-center items-center bg-red-50">
-      Hello from StudioTrap!
-    </div>
-  );
-}
