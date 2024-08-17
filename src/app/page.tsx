0d7ec1630790afd9b52d3759ae158ae2ebd6bbbf
Mauricio <mauriciorojascareers@gmail.com>
2024-08-17 18:17:52 -0500

    quotation bug for tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Main() {
   return (
-    <div className="flex justify-center h-screen items-center`">
+    <div className="flex justify-center h-screen items-center">
       Hello from StudioTrap!
     </div>
   );
