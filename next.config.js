f682e355be13526eeda2dec3bc14dd14e5268829
Mauricio <mauriciorojascareers@gmail.com>
2024-08-18 15:09:42 -0500

    seeing whatsup with nextconfig

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.ts");
+await import("./src/env.js");
 
 import { fileURLToPath } from "node:url";
 import createJiti from "jiti";
