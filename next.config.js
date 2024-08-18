987fa8e98cb4c06b7352cfd1063700ac45afd7c9
Mauricio <mauriciorojascareers@gmail.com>
2024-08-18 15:12:51 -0500

    fixed next config restart auth

--- next.config.js
+++ next.config.js
@@ -4,12 +4,6 @@
  */
 await import("./src/env.js");
 
-import { fileURLToPath } from "node:url";
-import createJiti from "jiti";
-const jiti = createJiti(fileURLToPath(import.meta.url));
-
-jiti("./app/env");
-
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
