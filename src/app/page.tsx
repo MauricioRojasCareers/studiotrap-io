760c520eb4ab16636cd444595c57ef5da77ae420
Mauricio <mauriciorojascareers@gmail.com>
2024-08-18 16:55:56 -0500

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,33 +12,27 @@ export default async function Home() {
 
   return (
     // <HydrateClient>
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Studio <span className="text-[hsl(280,100%,70%)]">Trap</span>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#ffd6ff] to-[#b8c0ff] text-white">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem] ">
+          Studio <span className="text-[#33134A]">Trap</span>
         </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8 bg-red-500">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8 ">
           <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20 h-30 w-60"
             href="https://create.t3.gg/en/usage/first-steps"
             target="_blank"
           >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
+            <h3 className="text-2xl font-bold">Studio →</h3>
+            <div className="text-lg">See Listings</div>
           </Link>
           <Link
             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
             href="https://create.t3.gg/en/introduction"
             target="_blank"
           >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
+            <h3 className="text-2xl font-bold">Music →</h3>
+            <div className="text-lg">See Vault</div>
           </Link>
         </div>
         <div className="flex flex-col items-center gap-2">
@@ -52,7 +46,7 @@ export default async function Home() {
             </p>
             <Link
               href={session ? "/api/auth/signout" : "/api/auth/signin"}
-              className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+              className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20 text-[#33134A]"
             >
               {session ? "Sign out" : "Sign in"}
             </Link>
