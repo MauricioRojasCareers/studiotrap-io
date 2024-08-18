24d132ae9478355b188a5e36a3b11a783ce0d907
Mauricio <mauriciorojascareers@gmail.com>
2024-08-17 22:25:26 -0500

    seeing if .env pushes

--- next.config.js
+++ next.config.js
@@ -2,7 +2,13 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.js");
+await import("./src/env.ts");
+
+import { fileURLToPath } from "node:url";
+import createJiti from "jiti";
+const jiti = createJiti(fileURLToPath(import.meta.url));
+
+jiti("./app/env");
 
 /** @type {import("next").NextConfig} */
 const config = {
