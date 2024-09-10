4cdb91034de997de3d4b0181bb34f1a9ba9ec38c
Sanath Boddhula <sanath.boddhula@gmail.com>
2024-09-10 13:51:44 -0500

    update listings form UI

--- prisma/migrations/20240910014214_create/migration.sql
+++ prisma/migrations/20240910014214_create/migration.sql
@@ -5,20 +5,17 @@ CREATE TABLE "Listing" (
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "title" TEXT NOT NULL,
     "description" TEXT NOT NULL,
-
     CONSTRAINT "Listing_pkey" PRIMARY KEY ("id")
 );
-
 -- CreateTable
 CREATE TABLE "ListingImage" (
     "id" TEXT NOT NULL,
     "listingId" TEXT NOT NULL,
-
     CONSTRAINT "ListingImage_pkey" PRIMARY KEY ("id")
 );
-
 -- AddForeignKey
-ALTER TABLE "Listing" ADD CONSTRAINT "Listing_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
+ALTER TABLE "Listing"
+ADD CONSTRAINT "Listing_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 -- AddForeignKey
-ALTER TABLE "ListingImage" ADD CONSTRAINT "ListingImage_listingId_fkey" FOREIGN KEY ("listingId") REFERENCES "Listing"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "ListingImage"
+ADD CONSTRAINT "ListingImage_listingId_fkey" FOREIGN KEY ("listingId") REFERENCES "Listing"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
