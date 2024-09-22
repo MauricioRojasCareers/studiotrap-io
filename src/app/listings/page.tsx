bca9eab975935a0b3e9198db6ea90ed74558aa76
Mauricio <mauriciorojascareers@gmail.com>
2024-09-21 23:11:07 -0500

    spinner on listings page

--- src/app/listings/page.tsx
+++ src/app/listings/page.tsx
@@ -5,7 +5,10 @@ import { motion } from "framer-motion"; // For animations
 import { useEffect, useState } from "react";
 import Spinner from "../components/Navbar/Spinner";
 import { useSession } from "next-auth/react";
-import { CircleX, Delete } from "lucide-react";
+import { CircleX } from "lucide-react";
+import Image from "next/image";
+
+import { AspectRatio } from "~/app/components/ui/aspect-ratio";
 
 interface Listing {
   User: {
@@ -20,11 +23,23 @@ interface Listing {
   createdAt: Date;
 }
 
+interface ImageFile {
+  Key: string;
+  LastModified: string;
+  ETag: string;
+  Size: number;
+  StorageClass: string;
+}
+
 export default function Listings() {
   const [listings, setListings] = useState<Listing[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const [files, setFiles] = useState<any[]>([]);
   const { data: session } = useSession();
+  const [images, setImages] = useState<{ Key: string }[]>([]);
+
+  const [imagesLoading, setImagesLoading] = useState(true);
 
   useEffect(() => {
     // Fetch listings from the API
@@ -37,7 +52,30 @@ export default function Listings() {
         const result = await response.json();
         if (!response.ok) return setError("Failed to fetch listings.");
         setListings(result.listings); // Set the fetched listings to state
+
+        const res = await fetch("/api/s3-read-images");
+        const imagesData = await res.json();
+
+        if (imagesData.success) {
+          setFiles(imagesData.files); // Store the list of files
+        } else {
+          setError("Failed to fetch files.");
+        }
+
         setError(null);
+
+        const responseImages = await fetch("/api/s3-read-images");
+        if (!responseImages.ok) {
+          throw new Error("Failed to fetch images");
+        }
+        const data = await responseImages.json();
+
+        const sortedImages = data.files.sort(
+          (a: ImageFile, b: ImageFile) =>
+            new Date(b.LastModified).getTime() -
+            new Date(a.LastModified).getTime()
+        );
+        setImages(sortedImages);
       } catch (error) {
         setError("An unexpected error occurred.");
         console.error(error);
@@ -85,63 +123,123 @@ export default function Listings() {
     );
   }
 
-  return (
-    <div className="min-h-screen w-full bg-gradient-to-b from-[#ffd6ff] to-[#b8c0ff]">
-      <motion.div
-        className="flex-grow p-6 flex flex-col items-center h-auto mb-16"
-        initial={{ opacity: 0, y: 50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8 }}
-      >
-        <h2 className="text-3xl font-bold text-center mb-6">Listings</h2>
+  function onImageLoad() {
+    setImagesLoading(false);
+  }
 
-        {listings.length > 0 ? (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-4xl w-full m-4 ">
-            {listings.map((listing) => (
-              <motion.div
-                className="p-6 bg-white rounded-lg shadow-lg transition-transform transform hover:scale-105 active:scale-110"
-                key={listing.id}
-                // whileHover={{ scale: 1.05 }}
-                // whileTap={{ scale: 0.95 }}
-              >
-                <div className="flex flex-col">
-                  <div className="flex flex-row justify-between items-center gap-4">
-                    <p className="text-md font-bold truncate">
-                      {listing.address}
-                      &nbsp;
-                    </p>
+  return (
+    <>
+      <div className="min-w-full bg-gradient-to-b from-[#ffd6ff] to-[#b8c0ff] flex flex-col p-8 gap-4">
+        <h2 className="text-center text-slate-700 font-bold text-2xl">
+          S3 Uploads
+        </h2>
+        <div className="grid grid-cols-auto md:grid-cols-2 lg:grid-cols-3 gap-12 md:p-8 lg:p-8">
+          {images?.map((image, index) => (
+            <div
+              className="bg-white rounded-md p-4 shadow-lg transition-transform transform hover:scale-105 active:scale-90 flex flex-col md:gap-6 lg:gap-6 gap-2"
+              key={index}
+            >
+              <div className="w-[100%] flex items-center">
+                <AspectRatio ratio={16 / 9} className="bg-muted">
+                  {!imagesLoading && (
+                    <div className="bg-black flex justify-center items-center h-full rounded-md">
+                      <Spinner width={100} height={100} />
+                    </div>
+                  )}
+                  <Image
+                    src={`https://studiotrap-images.s3.amazonaws.com/${image.Key}`}
+                    alt="Image"
+                    fill
+                    className="h-full w-full rounded-md object-cover"
+                    priority
+                    sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, (max-width: 1024px) 33vw, 25vw"
+                    onLoad={onImageLoad}
+                  />
+                </AspectRatio>
+              </div>
+              <p className="font-bold text-sm text-wrap truncate w-full text-center">
+                {image.Key}
+              </p>
+            </div>
+            // <AspectRatio ratio={16 / 9}>
+            //   <div
+            //     className="p-4 bg-white rounded-lg shadow-lg transition-transform transform hover:scale-105 active:scale-95 flex flex-col gap-4 justify-center w-[450px]"
+            //     key={index}
+            //   >
+            //     <Image
+            //       src={`https://studiotrap-images.s3.amazonaws.com/${image.Key}`} // Construct the S3 URL
+            //       alt={image.Key}
+            //       className="w-full h-auto object-cover rounded-md"
+            //       width={500}
+            //       height={300}
+            //       priority
+            //     />
+            //     <p className="font-bold text-xs">{image.Key}</p>
+            //   </div>
+            // </AspectRatio>
+          ))}
+        </div>
+      </div>
+      <div className="min-h-screen w-full bg-gradient-to-b from-[#ffd6ff] to-[#b8c0ff]">
+        <motion.div
+          className="flex-grow p-6 flex flex-col items-center h-auto mb-16"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+        >
+          <h2 className="text-3xl font-bold text-center mb-6">Listings</h2>
+
+          {listings.length > 0 ? (
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-4xl w-full m-4 ">
+              {listings.map((listing) => (
+                <motion.div
+                  className="p-6 bg-white rounded-lg shadow-lg transition-transform transform hover:scale-105 active:scale-110"
+                  key={listing.id}
+                  // whileHover={{ scale: 1.05 }}
+                  // whileTap={{ scale: 0.95 }}
+                >
+                  <div className="flex flex-row h-full gap-2 justify-between">
+                    <div className="flex flex-col w-[75%] h-20 ">
+                      <div className="">
+                        <h2 className="font-bold truncate">
+                          {listing.address}
+                          &nbsp;
+                        </h2>
+                      </div>
+                      <hr />
+                      <div className="h-full">
+                        <div className="absolute bottom-0 left-0 p-4">
+                          <p
+                            className="text-slate-900 text-sm truncate font-bold
+                    "
+                          >
+                            ${listing.pricePerHour}/
+                            <span className="font-extralight">hr</span>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+
                     {listing.User?.email === session?.user.email && (
-                      // <button
-                      //   onClick={() => onDelete(listing.id, listing.userId)}
-                      //   className="ml-auto p-2 bg-rose-500 w-8 h-8 text-xs text-white rounded-full cursor-pointer hover:bg-red-300 focus:outline-none"
-                      // >
-                      //   x
-                      // </button>
-                      <button className="">
+                      <button className="absolute top-0 right-0 p-2">
                         <div className="p-2">
                           <CircleX
-                            size={25}
-                            className="ml-auto md:text-slate-300 text-rose-400 hover:text-rose-500 ative:text-rose-800 transition-transform transform active:scale-75"
+                            size={20}
+                            className=" md:text-slate-300 text-rose-400 hover:text-rose-500 ative:text-rose-800 transition-transform transform active:scale-95 hover:scale-110"
                             onClick={() => onDelete(listing.id, listing.userId)}
                           />
                         </div>
                       </button>
                     )}
                   </div>
-                  <p
-                    className="text-slate-900 font-extralight text-sm truncate
-                    "
-                  >
-                    Price per hour: ${listing.pricePerHour}
-                  </p>
-                </div>
-              </motion.div>
-            ))}
-          </div>
-        ) : (
-          <p>No listings available.</p>
-        )}
-      </motion.div>
-    </div>
+                </motion.div>
+              ))}
+            </div>
+          ) : (
+            <p>No listings available.</p>
+          )}
+        </motion.div>
+      </div>
+    </>
   );
 }
