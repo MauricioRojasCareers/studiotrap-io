905a8f38840ef2044271885de801659b0c454865
Mauricio <mauriciorojascareers@gmail.com>
2024-09-28 15:45:02 -0500

    padding on dashboard

--- src/app/dashboard/MobileDashBoard.tsx
+++ src/app/dashboard/MobileDashBoard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function () {
   return (
-    <div className="bg-red-300 h-screen w-screen pt-20 pb-5 px-4 block md:hidden lg:hidden">
+    <div className="bg-red-300 h-screen w-screen px-4 block md:hidden lg:hidden pt-[4rem]">
       {/* Ensure the yellow container doesn't scroll */}
       <div className="bg-yellow-300 w-full text-black flex flex-col h-full">
         {/* Add padding to account for navbar height */}
