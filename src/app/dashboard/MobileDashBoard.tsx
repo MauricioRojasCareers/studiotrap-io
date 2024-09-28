90344f1fdb2e50e57289c96aa82c38883d736bf2
Mauricio <mauriciorojascareers@gmail.com>
2024-09-28 17:08:07 -0500

    overflow-y-auto

--- src/app/dashboard/MobileDashBoard.tsx
+++ src/app/dashboard/MobileDashBoard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function () {
   return (
-    <div className="bg-red-300 h-screen w-screen px-4 block md:hidden lg:hidden pt-[4rem] overflow-hidden">
+    <div className="bg-red-300 h-screen w-screen px-4 block md:hidden lg:hidden pt-[4rem] overflow-hidden overflow-y-auto">
       {/* Ensure the yellow container doesn't scroll */}
       <div className="bg-yellow-300 w-full text-black flex flex-col h-full p-4 overflow-hidden overflow-y-auto">
         {/* Add padding to account for navbar height */}
