6cfa18f3a63a4c13103abe15f773d00f013770b7
Mauricio <mauriciorojascareers@gmail.com>
2024-09-28 14:48:41 -0500

    different tailwind dashboard

--- src/app/dashboard/MobileDashBoard.tsx
+++ src/app/dashboard/MobileDashBoard.tsx
@@ -2,9 +2,11 @@ import React from "react";
 
 export default function () {
   return (
-    <div className="bg-red-300 h-screen w-screen pt-20 pb-5 px-4  block md:hidden lg:hidden">
-      <div className="bg-yellow-300 w-full flex-grow text-black flex mx-auto h-full">
-        <p>Hello from inside </p>
+    <div className="bg-red-300 h-screen w-screen pt-20 pb-5 px-4 block md:hidden lg:hidden">
+      {/* Ensure the yellow container doesn't scroll */}
+      <div className="bg-yellow-300 w-full text-black flex flex-col h-full">
+        {/* Add padding to account for navbar height */}
+        <p>Hello from inside</p>
       </div>
     </div>
   );
