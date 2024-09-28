82e6517a59645bb61d78f2549dfac6b1e2724bb2
Mauricio <mauriciorojascareers@gmail.com>
2024-09-28 16:59:39 -0500

    scrollable dashboard

--- src/app/dashboard/MobileDashBoard.tsx
+++ src/app/dashboard/MobileDashBoard.tsx
@@ -2,10 +2,56 @@ import React from "react";
 
 export default function () {
   return (
-    <div className="bg-red-300 h-screen w-screen px-4 block md:hidden lg:hidden pt-[4rem]">
+    <div className="bg-red-300 h-screen w-screen px-4 block md:hidden lg:hidden pt-[4rem] overflow-hidden">
       {/* Ensure the yellow container doesn't scroll */}
-      <div className="bg-yellow-300 w-full text-black flex flex-col h-full">
+      <div className="bg-yellow-300 w-full text-black flex flex-col h-full p-4 overflow-hidden overflow-y-auto">
         {/* Add padding to account for navbar height */}
+        <p>Hello from inside</p>
+        <p>Hello from inside</p>
+        <p>Hello from inside</p>
+        <p>Hello from inside</p>
+        <p>Hello from inside</p>
+        <p>Hello from inside</p>
+        <p>Hello from inside</p>
+
+        <p>Hello from inside</p>
+        <p>Hello from inside</p>
+        <p>Hello from inside</p>
+
+        <p>Hello from inside</p>
+        <p>Hello from inside</p>
+        <p>Hello from inside</p>
+
+        <p>Hello from inside</p>
+        <p>Hello from inside</p>
+
+        <p>Hello from inside</p>
+        <p>Hello from inside</p>
+        <p>Hello from inside</p>
+
+        <p>Hello from inside</p>
+        <p>Hello from inside</p>
+        <p>Hello from inside</p>
+
+        <p>Hello from inside</p>
+
+        <p>Hello from inside</p>
+        <p>Hello from inside</p>
+        <p>Hello from inside</p>
+        <p>Hello from inside</p>
+
+        <p>Hello from inside</p>
+        <p>Hello from inside</p>
+        <p>Hello from inside</p>
+        <p>Hello from inside</p>
+
+        <p>Hello from inside</p>
+        <p>Hello from inside</p>
+        <p>Hello from inside</p>
+        <p>Hello from inside</p>
+        <p>Hello from inside</p>
+
+        <p>Hello from inside</p>
         <p>Hello from inside</p>
       </div>
     </div>
